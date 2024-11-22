caa84b3f2b8a5f0df057b65d0dd98d7d07ab255e
sangwani <mightypz2@gmail.com>
2024-11-22 22:55:46 +0200

    Refactor: Extract navigation routes into a separate component for reusability and cleaner structure

--- src/components/Parents/ParentDashboard.js
+++ src/components/Parents/ParentDashboard.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { Link, Outlet } from "react-router-dom";
 import "../css/parentDashboard.css"; 
+import ParentLinks from "./ParentLinks";
+
 
 function ParentDashboard() {
   return (
@@ -8,26 +10,7 @@ function ParentDashboard() {
       {/* Sidebar */}
       <aside className="sidebar">
         <h2>Parent Dashboard</h2>
-        
-        <nav>
-          <ul>
-            <li>
-              <Link to="make-payment">Make a Payment</Link>
-            </li>
-            <li>
-              <Link to="payment-history">Payment History</Link>
-            </li>
-            <li>
-              <Link to="student-progress">Student Progress</Link>
-            </li>
-            <li>
-              <Link to="profile">Profile</Link>
-            </li>
-            <li>
-              <Link to="communication">Communication</Link>
-            </li>
-          </ul>
-        </nav>
+        <ParentLinks />
       </aside>
 
       {/* Main Content Area */}
---
+++ src/components/Parents/ParentLinks.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const ParentLinks = () => {
+    return (
+        <nav>
+            <ul>
+                <li>
+                    <Link to="make-payment">Make a Payment</Link>
+                </li>
+                <li>
+                    <Link to="payment-history">Payment History</Link>
+                </li>
+                <li>
+                    <Link to="student-progress">Student Progress</Link>
+                </li>
+                <li>
+                    <Link to="profile">Profile</Link>
+                </li>
+                <li>
+                    <Link to="communication">Communication</Link>
+                </li>
+            </ul>
+        </nav>
+    );
+};
+
+export default ParentLinks;
